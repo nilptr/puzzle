cfed36c3fdb7234a06baffd6a7740b63013447b5
nilptr <nilptr.js@gmail.com>
2016-03-03 15:14:48 +0800

    set.js 提取类型判断部分，实现 forEach

--- polyfills/set.js
+++ polyfills/set.js
@@ -1,81 +1,48 @@
 if (typeof Set === 'undefined') {
   (function () {
-    function Set(iterable) {
+    function Set(values) {
       if (!(this instanceof Set))
-        return new Set(iterable);
+        return new Set(values);
 
       // init
       this.clear();
 
-      var self = this;
-      if (typeof iterable === 'object' && iterable !== null) {
-        if (typeof iterable.forEach === 'function') {
-          iterable.forEach(function (item) {
-            self.add(item);
-          });
+      if (values) {
+        if (typeof values.forEach === 'function') {
+          values.forEach(function (v) {
+            this.add(v);
+          }, this);
         } else {
-          throw new TypeError(' is not iterable');
+          throw new TypeError(values + ' is not iterable');
         }
       }
     }
 
-    Set.prototype.add = function(val) {
-      switch (typeof val) {
-        case 'number':
-        case 'boolean':
-        case 'undefined':
-          if (!this._map[val])
-            this._map[val] = true;
-          break;
-        case 'string':
-          if (!this._map['$' + val])
-            this._map['$' + val] = true;
-          break;
-        case 'object':
-        case 'function':
-        default:
-          if (val === null && !this._map[val])
-            this._map[val] = true;
-          else if (this._arr.indexOf(val) < 0)
-            this._arr.push(val);
-          break;
-      }
+    Set.prototype.add = function(value) {
+      var val = transformValue(value);
+      if (isString(val))
+        this._map[val] = true;
+      else
+        this._arr.indexOf(val) < 0 && this._arr.push(val);
       return this;
     };
 
-    Set.prototype.delete = function(val) {
-      switch (typeof val) {
-        case 'number':
-        case 'boolean':
-        case 'undefined':
-          return this._map[val] && !(this._map[val] = false);
-        case 'string':
-          return this._map['$' + val] && !(this._map['$' + val] = false);
-        case 'object':
-        case 'function':
-        default:
-          var idx;
-          return val === null
-            ? (this._map[val] && !(this._map[val] = false))
-            : ((idx = this._arr.indexOf(val)) > 0 && this._arr.splice(idx, 1));
-      }
+    Set.prototype.delete = function(value) {
+      var val = transformValue(value);
+      var idx = null;
+      if (isString(val))
+        return this._map[val]
+          && !(this._map[val] = false);
+      else
+        return (idx = this._arr.indexOf(val)) >= 0
+          && this._arr.splice(idx, 1);
     };
 
-    Set.prototype.has = function(val) {
-      switch (typeof val) {
-        case 'number':
-        case 'boolean':
-        case 'undefined':
-          return !!this._map[val];
-        case 'string':
-          return !!this._map['$' + val];
-        case 'object':
-        case 'function':
-        default:
-          return val === null
-            ? !!this._map['null']
-            : this._arr.indexOf(val) > -1;
-      }
+    Set.prototype.has = function(value) {
+      var val = transformValue(value);
+      return isString(val)
+        ? this._map[val]
+        : this._arr.indexOf(val) >= 0;
     };
 
     Set.prototype.clear = function() {
@@ -90,5 +57,52 @@ if (typeof Set === 'undefined') {
     });
 
     Set.prototype.keys = Set.prototype.values = function () {};
+
+    Set.prototype.forEach = function (callback, thisArg) {
+      this._map.map(function (v) {
+          return restoreValue(v);
+      }).concat(this._arr).forEach(callback, thisArg);
+    };
+
+    function isString(val) {
+      return typeof val === 'string';
+    }
+
+    function transformValue(value) {
+      switch (typeof value) {
+        case 'number':
+        case 'boolean':
+        case 'undefined':
+          return '' + value;
+        case 'string':
+          return '$' + value;
+        case 'object':
+        case 'function':
+        default:
+          return value === null ? 'null' : value;
+      }
+    }
+
+    function restoreValue(value) {
+      if (isString(value)) {
+        switch (value) {
+          case 'null':
+            return null;
+          case 'NaN':
+            return NaN;
+          case 'undefined':
+            return undefined;
+          case 'true':
+          case 'false':
+            return value;
+          default:
+            if (value[0] === '$')
+              return value.slice(1);
+            else
+              return Number(value);
+        }
+      }
+      return value;
+    }
   })();
 }
